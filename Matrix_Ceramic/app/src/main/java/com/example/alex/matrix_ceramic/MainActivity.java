813a7608854040917b1e342878c6c635e897d90a
Alex.Falcon <alexfelkon@rambler.ru>
2019-05-13 21:19:18 +0300

    13.05.19

--- Matrix_Ceramic/app/src/main/java/com/example/alex/matrix_ceramic/MainActivity.java
+++ Matrix_Ceramic/app/src/main/java/com/example/alex/matrix_ceramic/MainActivity.java
@@ -2,18 +2,64 @@ package com.example.alex.matrix_ceramic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.*;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button add, find;
     EditText name, inventory, weight, rack;
-    AutoCompleteTextView type, pert;
+    AutoCompleteTextView type, part;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = (Button) findViewById(R.id.btn_add);
+        find = (Button)findViewById(R.id.btn_find);
+        name = (EditText) findViewById(R.id.et_name);
+        inventory = (EditText) findViewById(R.id.et_inventory);
+        weight = (EditText) findViewById(R.id.et_weight);
+        rack = (EditText) findViewById(R.id.et_rack);
+        type = (AutoCompleteTextView) findViewById(R.id.ac_type);
+        part = (AutoCompleteTextView) findViewById(R.id.ac_part);
+        part.setEnabled(false);
+
+        String[] arrType = getResources().getStringArray(R.array.arr_type);
+        String[] arrToilet = getResources().getStringArray(R.array.arr_toilet);
+        String[] arrWashbowl = getResources().getStringArray(R.array.arr_washbowl);
+
+        List<String> listWashbowl = Arrays.asList(arrWashbowl);
+        List<String> listToilet = Arrays.asList(arrToilet);
+        List<String> listType = Arrays.asList(arrType);
+
+        final ArrayAdapter<String> adapterType = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, listType);
+        final ArrayAdapter<String> adapterToilet = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, listToilet);
+        final ArrayAdapter<String> adapterWashbowl = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, listWashbowl);
+        type.setAdapter(adapterType);
+
+        type.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String temp = type.getText().toString();
+                if (temp.equals("унитаз")|| temp.equals("писуар")){
+                    part.setAdapter(adapterToilet);
+                    part.setEnabled(true);
+                }
+                else {
+                    part.setAdapter(adapterWashbowl);
+                    part.setEnabled(true);
+                }
+            }
+        });
+
+
+
+
     }
 }
--- Matrix_Ceramic/app/src/main/java/com/example/alex/matrix_ceramic/Matrix_DB.java
+++ Matrix_Ceramic/app/src/main/java/com/example/alex/matrix_ceramic/Matrix_DB.java
@@ -1,5 +1,6 @@
 package com.example.alex.matrix_ceramic;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,6 +9,8 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Alex on 10.05.2019.
  */
 public class Matrix_DB {
+    public static final String DATABASE = "product";
+    public static final int VERSION = 1;
     public static final String TABLE_NAME = "matrix";
     public static final String ID = "_id";
     public static final String NAME = "name";
@@ -16,6 +19,7 @@ public class Matrix_DB {
     public static final String INVENTORY = "inventory";
     public static final String WEIGHT = "weight";
     public static final String RACK = "rack";
+
     public static final String CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
             ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             NAME + " TEXT, " +
@@ -23,20 +27,58 @@ public class Matrix_DB {
             PART + " TEXT, " +
             INVENTORY + " INTEGER, " +
             WEIGHT + " INTEGER, " +
-            RACK + " INTEGER)";
+            RACK + " INTEGER);";
+
+    private final Context context;
+
+    public Matrix_DB(Context context) {
+        this.context = context;
+    }
+    private DBHelper dbHelper;
+
+    private SQLiteDatabase sqLiteDatabase;
+
+    //открыть подключение
+    public void open(){
+        dbHelper = new DBHelper(context, DATABASE, null, VERSION);
+        sqLiteDatabase = dbHelper.getWritableDatabase();
+    }
+
+    //закрыть подключение
+    public void close(){
+        if (dbHelper!=null)dbHelper.close();
+    }
+
+    //Добавить запись в таблицу
+    public void add(String name, String type, String part, String sInventory, String sWeight, String sRack){
+        ContentValues cv = new ContentValues();
+        int inventory, weight, rack;
+
+        inventory = (sInventory.equals(""))? 0: Integer.parseInt(sInventory);
+        weight = (sWeight.equals(""))? 0 : Integer.parseInt(sWeight);
+        rack = (sRack.equals(""))? 0 :Integer.parseInt(sRack);
+
+        cv.put(INVENTORY,inventory);
+        cv.put(NAME, name);
+        cv.put(TYPE, type);
+        cv.put(PART, part);
+        cv.put(WEIGHT, weight);
+        cv.put(RACK, rack);
+        sqLiteDatabase.insert(TABLE_NAME,null,cv);
+
+    }
+
 
 
     public class DBHelper extends SQLiteOpenHelper{
 
-        public static final String DATABASE = "product";
-        public static final int VERSION = 1;
-
         public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, DATABASE, factory, VERSION);
+            super(context, name, factory, version);
         }
 
         @Override
         public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            sqLiteDatabase.execSQL(CREATE);
 
         }
 
